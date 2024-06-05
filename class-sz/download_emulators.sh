90ac170ada0a4e40760358b7cf9c90160ae47f9e
borisbolliet <boris.bolliet@gmail.com>
2024-06-05 02:03:27 +0100

    Update download_emulators.sh

--- class-sz/download_emulators.sh
+++ class-sz/download_emulators.sh
@@ -24,36 +24,33 @@ find_cosmopower_dir() {
     find "$1" -maxdepth "$DEPTH" -type d -name "cosmopower-organization" -print -quit
 }
 
-# Check if PATH_TO_COSMOPOWER_ORGANIZATION is set
-if [ -z "$PATH_TO_COSMOPOWER_ORGANIZATION" ]; then
-    COSMOPOWER_DIR=$(find_cosmopower_dir "..")
 
-    if [ -z "$COSMOPOWER_DIR" ]; then
-        COSMOPOWER_DIR=$(find_cosmopower_dir ".")
-    fi
 
-    if [ -n "$COSMOPOWER_DIR" ]; then
-        echo "Found cosmopower-organization directory at: $(realpath "$COSMOPOWER_DIR")"
-        PATH_TO_COSMOPOWER_ORGANIZATION=$(realpath "$COSMOPOWER_DIR")
-    else
-        echo "--> cosmopower-organization directory not found within $DEPTH levels up or down."
-        echo "--> We will install it one level up!"
-        cd ..
-        mkdir cosmopower-organization
-        cd cosmopower-organization
-        git clone https://github.com/cosmopower-organization/lcdm.git
-        git clone https://github.com/cosmopower-organization/mnu.git
-        git clone https://github.com/cosmopower-organization/mnu-3states.git
-        git clone https://github.com/cosmopower-organization/ede.git
-        git clone https://github.com/cosmopower-organization/neff.git
-        git clone https://github.com/cosmopower-organization/wcdm.git
-        cd ..
-        PATH_TO_COSMOPOWER_ORGANIZATION=$(realpath "../cosmopower-organization")
-    fi
-else
-    echo "Using provided PATH_TO_COSMOPOWER_ORGANIZATION: $PATH_TO_COSMOPOWER_ORGANIZATION"
-    PATH_TO_COSMOPOWER_ORGANIZATION=$(realpath "$PATH_TO_COSMOPOWER_ORGANIZATION")
+COSMOPOWER_DIR=$(find_cosmopower_dir "..")
+
+if [ -z "$COSMOPOWER_DIR" ]; then
+    COSMOPOWER_DIR=$(find_cosmopower_dir ".")
 fi
+
+if [ -n "$COSMOPOWER_DIR" ]; then
+    echo "Found cosmopower-organization directory at: $(realpath "$COSMOPOWER_DIR")"
+    PATH_TO_COSMOPOWER_ORGANIZATION=$(realpath "$COSMOPOWER_DIR")
+else
+    echo "--> cosmopower-organization directory not found within $DEPTH levels up or down."
+    echo "--> We will install it one level up!"
+    cd ..
+    mkdir cosmopower-organization
+    cd cosmopower-organization
+    git clone https://github.com/cosmopower-organization/lcdm.git
+    git clone https://github.com/cosmopower-organization/mnu.git
+    git clone https://github.com/cosmopower-organization/mnu-3states.git
+    git clone https://github.com/cosmopower-organization/ede.git
+    git clone https://github.com/cosmopower-organization/neff.git
+    git clone https://github.com/cosmopower-organization/wcdm.git
+    cd ..
+    PATH_TO_COSMOPOWER_ORGANIZATION=$(realpath "../cosmopower-organization")
+fi
+
 
 # Write environment setup to file
 echo "export PATH_TO_COSMOPOWER_ORGANIZATION=${PATH_TO_COSMOPOWER_ORGANIZATION}" > "${MDIR}/../set_class_sz_env.sh"
