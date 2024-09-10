a4ed99066bd326297cefaccf02be5c83596733d9
Yogesh <ymehta3@niagara>
2024-09-10 01:44:35 -0400

    updated python wrapper to reflect new param names

--- python/classy.pyx
+++ python/classy.pyx
@@ -995,12 +995,14 @@ cdef class Class:
               self.tsz.beta_cib = pdict_to_update['Emissivity_index_of_sed']
           if k == 'Power_law_index_of_SED_at_high_frequency':
               self.tsz.gamma_cib = pdict_to_update['Power_law_index_of_SED_at_high_frequency']
-          if k == 'Redshift_evolution_of_L_-_M_normalisation':
-              self.tsz.delta_cib = pdict_to_update['Redshift_evolution_of_L_-_M_normalisation']
+          if k == 'Redshift_evolution_of_L_M_normalisation':
+              self.tsz.delta_cib = pdict_to_update['Redshift_evolution_of_L_M_normalisation']
           if k == 'Most_efficient_halo_mass_in_Msun':
               self.tsz.L0_cib = pdict_to_update['Most_efficient_halo_mass_in_Msun']
           if k == 'Size_of_halo_masses_sourcing_CIB_emission':
               self.tsz.sigma2_LM_cib = pdict_to_update['Size_of_halo_masses_sourcing_CIB_emission']
+          if k == 'Normalisation_of_L_M_relation_in_[JyMPc2/Msun]':
+              self.tsz.sigma2_LM_cib = pdict_to_update['Normalisation_of_L_M_relation_in_[JyMPc2/Msun]']
         # print('array_redshift:',
         #       self.tsz.array_redshift[0],
         #       self.tsz.array_redshift[1],
