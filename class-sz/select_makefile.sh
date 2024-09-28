141e98a8656fb2c231c436f94b577b2f4510fd8b
Boris Bolliet <boris.bolliet@gmail.com>
2024-09-28 17:45:54 +0100

    Update select_makefile.sh

--- class-sz/select_makefile.sh
+++ class-sz/select_makefile.sh
@@ -4,10 +4,15 @@
 UNAME_S=$(uname -s)
 UNAME_M=$(uname -m)
 
+echo "UNAME_S: $UNAME_S"
+echo "UNAME_M: $UNAME_M"
+
 # Select the appropriate Makefile
 if [ "$UNAME_S" == "Darwin" ] && [ "$UNAME_M" == "arm64" ]; then
+    echo "using M1 Makefile"
     cp Makefile_m1 Makefile
 elif [ "$UNAME_S" == "Linux" ]; then
+    echo "using Linux Makefile"
     cp Makefile_linux Makefile
 else
     echo "OS not supported yet: $UNAME_S. See README.md for preM1 mac. Otherwise, please get in touch or open an issue on the CLASS-SZ github."
