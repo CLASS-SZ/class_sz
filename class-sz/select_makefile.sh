9b6f20d3b2bc4c2b4719030728d96c2bb609edb7
Boris Bolliet <boris.bolliet@gmail.com>
2024-09-29 14:56:12 +0100

    Update select_makefile.sh

--- class-sz/select_makefile.sh
+++ class-sz/select_makefile.sh
@@ -14,6 +14,21 @@ if [ "$UNAME_S" == "Darwin" ] && [ "$UNAME_M" == "arm64" ]; then
 elif [ "$UNAME_S" == "Linux" ]; then
     echo "using Linux Makefile"
     cp Makefile_linux Makefile
+    
+    # Print contents of /usr/include/ and /usr/lib/x86_64-linux-gnu/
+    echo "Contents of /usr/include/:"
+    ls -la /usr/include/
+    
+    echo "Contents of /usr/lib/x86_64-linux-gnu/:"
+    ls -la /usr/lib/x86_64-linux-gnu/
+    
+    # Print environment variables
+    echo "Environment Variables:"
+    echo "C_INCLUDE_PATH: $C_INCLUDE_PATH"
+    echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+    echo "LIBRARY_PATH: $LIBRARY_PATH"
+    echo "CFLAGS: $CFLAGS"
+    echo "LDFLAGS: $LDFLAGS"
 else
     echo "OS not supported yet: $UNAME_S. See README.md for preM1 mac. Otherwise, please get in touch or open an issue on the CLASS-SZ github."
     echo "UNAME_S: $UNAME_S"
