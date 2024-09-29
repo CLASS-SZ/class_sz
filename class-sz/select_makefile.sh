b433772ac8d21b0d563e664e82a1dbb79518e248
Boris Bolliet <boris.bolliet@gmail.com>
2024-09-29 19:07:58 +0100

    Update select_makefile.sh

--- class-sz/select_makefile.sh
+++ class-sz/select_makefile.sh
@@ -9,18 +9,11 @@ echo "UNAME_M: $UNAME_M"
 
 # Select the appropriate Makefile
 if [ "$UNAME_S" == "Darwin" ] && [ "$UNAME_M" == "arm64" ]; then
-    echo "using M1 Makefile"
+    echo "Using M1 Makefile"
     cp Makefile_m1 Makefile
 elif [ "$UNAME_S" == "Linux" ]; then
-    echo "using Linux Makefile"
+    echo "Using Linux Makefile"
     cp Makefile_linux Makefile
-    
-    # Print contents of /usr/include/ and /usr/lib/x86_64-linux-gnu/
-    #echo "Contents of /usr/include/:"
-    #ls -la /usr/include/
-    
-    #echo "Contents of /usr/lib/x86_64-linux-gnu/:"
-    #ls -la /usr/lib/x86_64-linux-gnu/
     
     # Print environment variables
     echo "Environment Variables:"
@@ -29,11 +22,20 @@ elif [ "$UNAME_S" == "Linux" ]; then
     echo "LIBRARY_PATH: $LIBRARY_PATH"
     echo "CFLAGS: $CFLAGS"
     echo "LDFLAGS: $LDFLAGS"
+elif [[ "$UNAME_S" == *"MINGW"* || "$UNAME_S" == *"CYGWIN"* || "$UNAME_S" == *"MSYS"* ]]; then
+    echo "Using Windows Makefile"
+    cp Makefile_windows Makefile
+    
+    # Optionally, print some environment variables if needed for Windows
+    echo "Environment Variables:"
+    echo "C_INCLUDE_PATH: $C_INCLUDE_PATH"
+    echo "LIBRARY_PATH: $LIBRARY_PATH"
+    echo "CFLAGS: $CFLAGS"
+    echo "LDFLAGS: $LDFLAGS"
 else
-    echo "OS not supported yet: $UNAME_S. See README.md for preM1 mac. Otherwise, please get in touch or open an issue on the CLASS-SZ github."
+    echo "OS not supported yet: $UNAME_S. See README.md for pre-M1 mac. Otherwise, please get in touch or open an issue on the CLASS-SZ github."
     echo "UNAME_S: $UNAME_S"
     echo "UNAME_M: $UNAME_M"
-    echo "will try with Makefile_m1x"
+    echo "Will try with Makefile_m1x"
     cp Makefile_m1 Makefile
 fi
-
